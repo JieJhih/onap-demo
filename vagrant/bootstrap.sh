c87b0f171b1d54091f8b83e685a08645504761ec
denverwilliams <denver@debian.nz>
2018-05-11 10:06:04 +0000

    Configure the br-ex to use 192.168.0.254/24 as the default gw

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,11 +8,12 @@ cd devstack; git checkout stable/ocata
 #patch -p1 < /vagrant/patches/pip_version_check_fix.patch 
 sudo apt-get install openvswitch-switch -y
 sudo ovs-vsctl add-br br-ex
-netif=$(ip a | grep '192.168.240' | awk '{print $NF}')
+netif=$(ip a | grep '192.168.0' | awk '{print $NF}')
 echo $netif
 inet=$(ip a | grep "inet.*$netif" | cut -f6 -d' ')
 sudo ip addr flush $netif
 sudo ovs-vsctl add-port br-ex $netif
 sudo ifconfig br-ex $inet up
-sudo ip route add default via 192.168.240.2
+sudo ip route del default
+sudo ip route add default via 192.168.0.254
 echo "source /vagrant/openrc" >> $HOME/.bash_profile
