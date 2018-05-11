77a21e80efbf905183490100b52fd72e890af350
denverwilliams <denver@debian.nz>
2018-05-11 09:44:19 +0000

    Add nova-manage cell_v2 simple_cell_setup & switch to using 192.168.0.0/24 nova-manage cell_v2 simple_cell_setup must now be run, as part of Ocata, nova has made the cell_v2 setup manditory for the nova-api db sync process. https://bugs.launchpad.net/tripleo/+bug/1649341 https://docs.openstack.org/nova/latest/user/cells.html We are also now using the 192.168.0.0/24 as the public network to assign floating ips from.

--- vagrant/setup_control.sh
+++ vagrant/setup_control.sh
@@ -8,7 +8,8 @@ cp /vagrant/control.conf  local.conf
 source /vagrant/openrc
 
 openstack network create public --external --provider-network-type=flat --provider-physical-network=public
-openstack subnet create --network=public --subnet-range=192.168.1.0/24 --allocation-pool start=192.168.1.200,end=192.168.1.250 --gateway 192.168.1.1 public-subnet
+openstack subnet create --network=public --subnet-range=192.168.0.0/24 --allocation-pool start=192.168.0.200,end=192.168.0.250 --gateway 192.168.0.254 public-subnet
+#openstack subnet create --network=public --subnet-range=192.168.1.0/24 --allocation-pool start=192.168.1.200,end=192.168.1.250 --gateway 192.168.1.1 public-subnet
 openstack security group list -f value | cut -f1 -d" " | xargs -I {} openstack security group rule create --ingress --ethertype=IPv4 --protocol=0 {}
 
 wget https://cloud-images.ubuntu.com/releases/14.04.1/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
@@ -26,3 +27,11 @@ openstack flavor delete m1.xlarge
 openstack flavor create --public m1.xlarge --id auto --ram 6144 --vcpus 4 --disk 100
 openstack flavor delete m1.xxlarge || true
 openstack flavor create --public m1.xxlarge --id auto --ram 8196 --vcpus 4 --disk 100
+
+#This is now required in the Ocata release for node placment.
+#https://bugs.launchpad.net/nova/+bug/1693979
+#https://docs.openstack.org/releasenotes/nova/ocata.html#upgrade-notes
+
+echo "After the computer nodes are up, SSH into the control and run the following:"
+echo -e "\tnova-manage cell_v2 simple_cell_setup"
+echo "Note: this needs to happen before the ONAP nodes are brought up"
