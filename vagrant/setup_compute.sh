7b0aada18415c6601393626c2cf641c530cc66cc
JieJhih Jhang <aawer12345tw@yahoo.com.tw>
2019-07-01 21:17:34 +0800

    fix ip

--- vagrant/setup_compute.sh
+++ vagrant/setup_compute.sh
@@ -4,7 +4,7 @@ set -ex
 cd devstack
 cp /vagrant/compute.conf local.conf
 host=$(hostname)
-ip=$(ifconfig | grep 192.168.0 | cut -f2 -d: | cut -f1 -d' ')
+ip=$(ifconfig | grep 192.168.0 | cut -f2 -d: | cut -f1 -d' '| head -1)
 sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
 ./stack.sh
 
